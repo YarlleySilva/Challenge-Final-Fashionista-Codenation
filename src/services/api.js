4c8c457e96e5ab973eac9a6044db6a86e21a2218
Yarlley Silva <yarlley.silva@gmail.com>
2020-07-09 03:03:58 -0300

    Fix API Link

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
-const baseUrl = "https://5e9935925eabe7001681c856.mockapi.io/api/v1/catalog";
+// const baseUrl = "https://5e9935925eabe7001681c856.mockapi.io/api/v1/catalog"; <<< não está funcionando
+
+// const baseUrl = "https://undefined.netlify.app/api/catalog";  <<< Caso não funcione o de baixo, utilizar este
+
+const baseUrl = "https://api.jsonbin.io/b/5f06b199a62f9b4b276192e4";
 
 export default async function api() {
   return await axios.get(baseUrl);
